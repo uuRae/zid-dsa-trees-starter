f792d3b4d29fe1968252d4ee1adabad1a632c8b8
Katrina Pingryn <katep@indeed.com>
2024-03-09 16:21:53 -0600

    solving problems

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -172,6 +172,60 @@ class BinarySearchTree {
     return values;
   }
 
+  getHeight(currentHeight = 0) {
+    // BASE CASE:
+    // If the current node doesn't have a left or right child,
+    // then the base case is reached, and the function can return the height.
+    if (!this.left && !this.right) return currentHeight;
+
+    // RECURSIVE CASE:
+    // Otherwise, compute the new height.
+    const newHeight = currentHeight + 1;
+
+    // If there's no left child, recurse down the right subtree only,
+    // passing down the height of the current node.
+    if (!this.left) return this.right.getHeight(newHeight);
+
+    // If there's no right child, recurse down the left subtree only,
+    // passing down the height of the current node.
+    if (!this.right) return this.left.getHeight(newHeight);
+
+    // If both children exist, recurse down both subtrees,
+    // passing down the height of the current node.
+    const leftHeight = this.left.getHeight(newHeight);
+    const rightHeight = this.right.getHeight(newHeight);
+
+    // Return the greater of the left or right subtree heights.
+    return Math.max(leftHeight, rightHeight);
+  }
+
+  isBST() {
+    // Use the existing `dfsInOrder()` method to traverse the tree.
+    const values = this.dfsInOrder();
+
+    // Check if the array returned by the in-order DFS is a sorted array.
+    for (let i = 1; i < values.length; i++) {
+      // Compare the current and previous values.
+      if (values[i] < values[i - 1]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  findKthLargestValue(k) {
+    // Use the existing `dfsInOrder()` method to traverse the tree.
+    const values = this.dfsInOrder();
+    const kthIndex = values.length - k;
+
+    // Ensure that the index is within the bounds of the array.
+    if (kthIndex >= 0) {
+      return values[kthIndex];
+    } else {
+      console.error("k value exceeds the size of the BST.");
+    }
+  }
+
   _replaceWith(node) {
     if (this.parent) {
       if (this == this.parent.left) {
